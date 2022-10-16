02084209e1012b844209142ec98053c5a1359a68
wellingtonpleni <wellingtonpleni@gmail.com>
2022-10-15 22:07:39 -0300

    Configurações

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-/**
- * @format
- */
+import React from 'react';
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Home from './src/pages/Home';
 import {name as appName} from './app.json';
+import {fonts} from './src/styles/theme.json';
+
+import {Provider as StoreProvider} from 'react-redux';
+import {
+  DefaultTheme,
+  configureFonts,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+
+import store from './src/store';
+
+const theme = {
+  ...DefaultTheme,
+  fonts: configureFonts({
+    ios: fonts,
+    android: fonts,
+  }),
+};
+
+const App = () => {
+  return (
+    <StoreProvider store={store}>
+      <PaperProvider theme={theme}>
+        <Home />
+      </PaperProvider>
+    </StoreProvider>
+  );
+};
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/consts.js
@@ -0,0 +1,5 @@
+export default {
+    salaoId: '609310a1002ab333d1ae1716',
+    clienteId: '60946ad8e400c50be68dc904',
+    bucketUrl: 'https://salao-na-mao-dev.s'
+}
